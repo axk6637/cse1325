50bcf1ef6009621d0e39c825f2fd54244adbefc5
Aashreeya Karmacharya <axk6637@mavs.uta.edu>
2024-08-23 23:25:21 -0500

    Hello.java

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Hello{
     public static void main (String[] args){
-        System.out.println("Hello, Aashreeya");
+        Scanner in= new Scanner (System.in);
+        System.out.println("Enter your name: ");
+        String userName;
+        userName= in.nextLine();
+        System.out.println("Hello,"+ " "+ userName);
+
     }
 }
