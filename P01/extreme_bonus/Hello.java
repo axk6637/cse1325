46d48e408a04022b6e4c147c8bdb142e30c33314
Aashreeya Karmacharya <axk6637@mavs.uta.edu>
2024-08-26 16:54:17 -0500

    P01 extreme_bonus

---
+++ P01/extreme_bonus/Hello.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Hello 
+{
+    public static void main(String[] args){
+        String userName= System.getProperty("user.name");
+        System.out.println("Hello," + userName);
+        
+    }
+  
+}
