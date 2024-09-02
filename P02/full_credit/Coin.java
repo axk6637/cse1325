7e89cf0792bdafc27dba3e608fec8672621b18c8
Aashreeya Karmacharya <axk6637@mavs.uta.edu>
2024-09-02 01:54:14 -0500

    P02 full_credit

--- P02/full_credit/Coin.java
+++ P02/full_credit/Coin.java
@@ -10,5 +10,18 @@ public class Coin {
         this.year= year;
     }
 
-    
+    // Method
+    public double getValue(){
+        return switch(denomination){
+         case PENNY -> 0.01;
+         case NICKEL -> 0.05;
+         case DIME -> 0.10;
+         case QUARTER -> 0.25;
+        } ;
+     }
+     
+     // Method
+     public int getYear(){
+         return year;
+     }
 }
