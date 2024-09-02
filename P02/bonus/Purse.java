9ce72179da719726f0082cae5e593903305f1f1a
Aashreeya Karmacharya <axk6637@mavs.uta.edu>
2024-09-02 09:47:04 -0500

    P02_BONUS

---
+++ P02/bonus/Purse.java
@@ -0,0 +1,37 @@
+public class Purse {
+
+     //Array of Coin containing 5 coins with Coin object and dates
+    public static void main(String[] args){
+        Coin[] purse={
+            new Coin(Denomination.PENNY, 1924),
+            new Coin(Denomination.NICKEL, 1945),
+            new Coin(Denomination.DIME, 1990),
+            new Coin(Denomination.QUARTER, 2010),
+    
+        };
+    
+        double totalValue= 0.0;
+        int earliestYear= Integer.MAX_VALUE;
+        int latestYear = Integer.MIN_VALUE;
+    
+        for (Coin coin: purse){
+            totalValue +=coin.getValue();
+            System.out.println(coin);
+            int year =coin.getYear();
+    
+            if (year < earliestYear){
+                earliestYear =year;
+            }
+            if (year> latestYear){
+                latestYear= year;
+            }
+        }
+    
+            // Print Statements
+    
+        System.out.printf("The total value of the coins in the purse is $%.2f!\n", totalValue);
+        System.out.println("The coins are from the year: " + earliestYear + " to " + latestYear + "!");
+    
+    }
+}
+
