1ed36c54aea080160842c3e9b849281b2c9d4559
Aashreeya Karmacharya <axk6637@mavs.uta.edu>
2024-09-02 01:51:31 -0500

    P02 full_credit

---
+++ P02/full_credit/Coin.java
@@ -0,0 +1,14 @@
+public class Coin {
+    // Private fields
+
+    private Denomination denomination;
+    private int year;
+
+    // Constructor
+    public Coin(Denomination denomination, int year){
+        this.denomination = denomination;
+        this.year= year;
+    }
+
+    
+}
