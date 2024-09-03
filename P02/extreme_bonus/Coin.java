40555a10d862d43466806a9a5b096000266b4b91
Aashreeya Karmacharya <axk6637@mavs.uta.edu>
2024-09-02 22:52:23 -0500

    P02_extreme_bonus

--- P02/extreme_bonus/Coin.java
+++ P02/extreme_bonus/Coin.java
@@ -9,4 +9,53 @@ public class Coin {
         this.denomination = denomination;
         this.year= year;
     }
+
+    // Method
+    public double getValue(){
+        return denomination.getValue();
+     }
+     
+     // Method
+     public int getYear(){
+         return this.year;
+     }
+
+     //Method
+     public double getWeight(){
+     switch(denomination){
+        case PENNY:
+            if (year < 1983){
+                return 3.110;
+            } 
+            else{
+                return 2.500;
+            }
+        case NICKEL:
+            return 5.000;
+        case DIME:
+            if (year <1965) {
+                return 2.500;
+            }
+            else{
+                return 2.268;
+            }
+        case QUARTER:
+            if (year <1965){
+                return 6.250;
+            }
+            else{
+                return 5.670;
+            }
+        default:
+            return 0.0;
+        }
+     }
+     
+
+     //Overrride
+     @Override
+     public String toString() {
+        return year + " " + denomination.toString();
+     }
+
 }
