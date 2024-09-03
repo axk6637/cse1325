0bf1d0400ff81749541fcd638c8aa07dc975d892
Aashreeya Karmacharya <axk6637@mavs.uta.edu>
2024-09-02 23:00:39 -0500

    P02_extreme_bonus

--- P02/extreme_bonus/Purse.java
+++ P02/extreme_bonus/Purse.java
@@ -11,5 +11,29 @@ public class Purse {
 
         };
 
-}
+        double totalValue= 0.0;
+        double totalWeight=0.0;
+        int earliestYear= Integer.MAX_VALUE;
+        int latestYear = Integer.MIN_VALUE;
+
+        for (Coin coin: purse){
+            totalValue +=coin.getValue();
+            totalWeight +=coin.getWeight();
+            int year =coin.getYear();
+
+            if (year < earliestYear){
+                earliestYear =year;
+            }
+            if (year> latestYear){
+                latestYear= year;
+            }
+        }
+
+        // Print Statements
+
+        System.out.printf("The total value of the coins in the purse is $%.2f!\n", totalValue);
+        System.out.printf("The total weight of your coins is %.3f!\n", totalWeight);
+        System.out.println("The coins are from the year: " + earliestYear + " to " + latestYear + "!");
+
+    }
 }
