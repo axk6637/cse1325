6f49473cd7a075effdc78cfd9705a5ac7fd97137
Aashreeya Karmacharya <axk6637@mavs.uta.edu>
2024-09-03 00:49:25 -0500

    P02 extreme_bonus

--- P02/extreme_bonus/Coin.java
+++ P02/extreme_bonus/Coin.java
@@ -26,35 +26,35 @@ public class Coin {
             case PENNY:
                 if (year < 1983){
                     return 3.110;
-            } 
+                } 
                 else if (year >1984){
                     return 2.500;
-            }
+                }
                 else{
                     return 0.0;
-            }
+                }   
             case NICKEL:
                 return 5.000;
             case DIME:
                 if (year <1965) {
                     return 2.500;
-            }
+                }
                 else if (year >1966){
                     return 2.268;
-            }
+                }
                 else{
                     return 0.0;
-            }
+                }
             case QUARTER:
                 if (year <1965){
                     return 6.250;
-            }
+                }
                 else if (year > 1966){
                     return 5.670;
-            }
+                }
                 else{
                     return 0.0;
-            }
+                }
         default:
             return 0.0;
         }
