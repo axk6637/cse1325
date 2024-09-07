9189ce58d28975661dc9876cb135d22ba74d0c27
Aashreeya Karmacharya <axk6637@mavs.uta.edu>
2024-09-07 16:30:08 -0500

    P03

---
+++ P03/Media.java
@@ -0,0 +1,16 @@
+public class Media{
+    private String title;
+    private String url;
+
+    public Media(String title, String url){
+        this.title=title;
+        this.url=url;
+    }
+
+    //Overrride
+    @Override
+    public String toString(){
+        return title + "(" + url + ")";
+    }
+
+}
