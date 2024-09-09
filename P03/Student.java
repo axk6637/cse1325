2d9ea848e984ac8114794518a481eeac4b282c77
Aashreeya Karmacharya <axk6637@mavs.uta.edu>
2024-09-08 23:54:11 -0500

    P03

---
+++ P03/Student.java
@@ -0,0 +1,31 @@
+public class Student {
+
+    //private fields
+    private String name;
+    private int id;
+    private String email;
+    private Account account;
+
+    //Method 1
+    public Student(String name, int id, String email){
+        if(!email.toLowerCase().endsWith("uta.edu") && !email.toLowerCase().endsWith("@mavs.uta.edu")){
+            throw new IllegalArgumentException("Non-UTA email address"+ email);
+        }
+        this.name=name;
+        this.id=id;
+        this.email=email;
+        //newly instanced Account
+        this.account= new Account();
+    }   
+
+    public String requestMedia(Media media){
+        return account.play(media);
+    }
+
+    //Override
+    @Override
+    public String toString(){
+        return name + "(" + id + ","+ email +"," + "Account #" account.getAccountNumber() + ")" ;
+    }
+
+}
---
+++ P03/TestStudent.java
@@ -0,0 +1,11 @@
+public class TestStudent{
+    public static void main(String[] args) {
+        int result=0; 
+        //1st vector test
+        try{
+            Student student1 = new Student("Prof Rice", 1234567, "george.rice@uta.edu");
+            
+        }
+
+    }
+}
