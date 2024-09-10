3776c2103bcf6cda172a92024afba7ebb504e20c
Aashreeya Karmacharya <axk6637@mavs.uta.edu>
2024-09-09 23:08:03 -0500

    P03

--- P03/TestStudent.java
+++ P03/TestStudent.java
@@ -16,54 +16,32 @@ public class TestStudent{
             totalErrors++;
        }
 
-        // 2nd Vector test for non-UTA emails
-        try{
-            Student student2= new Student("Bill Gates", 123, "bill@gmail.com");
+       // 2nd Vector test for non-UTA emails
+       try{
+        Student student2= new Student("Bill Gates", 123, "bill@gmail.com");
 
-            //Condition 1: Report as an error if NO exception being thrown
-            throw new IllegalArgumentException("Expected IllegalArgumentException but not thrown");
+        //Condition 1: Report as an error if NO exception being thrown
+        throw new IllegalArgumentException("Expected IllegalArgumentException but not thrown");
         }catch (IllegalArgumentException e){
 
-            //Catch and ignore the correct exception
-            String expectedMessage = "Non-UTA email address: bill@gmail.com";
-            if(!e.getMessage().equals(expectedMessage)){
+        //Catch and ignore the correct exception
+        String expectedMessage = "Non-UTA email address: bill@gmail.com";
+        if(!e.getMessage().equals(expectedMessage)){
 
-                //Report as an error any other exception message
-                System.err.println("FAIL! Incorrect exception messgae: "+ e.getMessage());
-                System.exit(-1);
-                totalErrors++;
-            }
-        }catch (Exception e){
-
-            //Report as an error any other exception
-            System.err.println("FAIL! Unexpected exception type: "+ e.getClass().getName());
+            //Report as an error any other exception message
+            System.err.println("FAIL! Incorrect exception messgae: "+ e.getMessage());
             System.exit(-1);
             totalErrors++;
         }
+        }catch (Exception e){
 
-        //3rd vector test
-        try{
-            Student student3 = new Student("Ash Kar", 6637, "axk6637@mavs.uta.edu");
-            Media media= new Media("Example Title", "https://example.com");
-            String expectedMediaReq= "Playing Example Title (https://example.com)";
+        //Report as an error any other exception
+        System.err.println("FAIL! Unexpected exception type: "+ e.getClass().getName());
+        System.exit(-1);
+        totalErrors++;
+    }
 
-            if (!student3.requestMedia(media).equals(expectedMediaReq)) {
-                throw new IllegalArgumentException("FAIL! requestMedia() returned incorrect output");
-            }
-        } catch (IllegalArgumentException e) {
-            System.err.println(e.getMessage());
-            totalErrors++; 
-        } catch (Exception e) {
-            //Any other exception
-            System.err.println("FAIL: Unexpected exception: " + e.getMessage());
-            totalErrors++; 
-            }
-    if (totalErrors>0){
-        System.exit(totalErrors);
-    }else{
-        System.exit(0);
-        }
-
+    
     }
 }
 
