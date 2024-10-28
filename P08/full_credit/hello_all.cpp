ef1420ec3bdf4056982a51b41132c4e95e129075
Aashreeya Karmacharya <axk6637@mavs.uta.edu>
2024-10-27 22:17:22 -0500

    full_credit

---
+++ P08/full_credit/hello_all.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main(){
+    std::string name;
+    std:: cout << "Please Enter Your Name: ";
+    std::cin >>name;
+
+    std::cout << "Hello, " <<name << "!" <<std::endl;
+    return 0;
+}
