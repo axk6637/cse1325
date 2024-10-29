50ab9154e136c81d1a2e18f6477130a2a3d5f176
Aashreeya Karmacharya <axk6637@mavs.uta.edu>
2024-10-28 22:39:01 -0500

    extreme bonus

---
+++ P08/extreme_bonus/nums.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+#include <algorithm>
+#include <random>
+#include <chrono>
+
+
+int main(int argc, char* argv[]){
+    std::vector <std::string> numbers; //Stackbased vector
+    std::vector<std::string>* words= new std::vector<std::string>();
+
+    for (int i=1; i<argc; ++i){
+        std::string arg=argv[i];
+
+        if(std::isdigit(arg[0])){
+            numbers.push_back(arg);
+        }else{
+            words->push_back(arg);
+        }
+    }
+
+    std::cout << "Numbers: " <<std::endl;
+    for(auto num: numbers){
+           std::cout <<num << std::endl; 
+    }
+
+    std::cout << "Words: " << std::endl;
+    for(auto word: *words){
+        std::cout <<word <<std::endl;
+    }
+
+    delete words; 
+    return 0;
+
+}
