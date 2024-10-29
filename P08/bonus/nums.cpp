73dde2903228cf37cf718f60eeb29f1eeba9d30c
Aashreeya Karmacharya <axk6637@mavs.uta.edu>
2024-10-28 20:34:23 -0500

    bonus

---
+++ P08/bonus/nums.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cctype>
+
+int main(int argc, char* argv[]){
+    std::vector <std::string> numbers; //Stackbased vector
+    std::vector<std::string>* words= new std::vector<std::string>();
+
+    for (int i=1; i<argc; ++i){
+        std::string arg=argv[i];
+
+        if(std::isdigit(arg[0])){
+            numbers.push_back(arg);
+        }else{
+            words->push_back(arg);
+        }
+    }
+
+    
