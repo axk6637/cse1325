8447386f6439df1bce5c9a09b0e37a75d854840d
Aashreeya Karmacharya <axk6637@mavs.uta.edu>
2024-11-02 13:59:35 -0500

    full credit

--- P09/clock.h
+++ P09/clock.h
@@ -4,3 +4,18 @@
 #include <iostream>
 #include <stdexcept>
 #include <iomanip>
+class Clock {
+private:
+    int _hours;
+    int _minutes;
+    int _seconds;
+
+public:
+    Clock(int hours, int minutes, int seconds);
+    virtual ~Clock() {} 
+
+    void tic();
+    void print() const;
+};
+
+#endif // CLOCK_H
