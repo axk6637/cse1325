6e27488e3abb1fb52c10de29a42ed08e330b2c5c
Aashreeya Karmacharya <axk6637@mavs.uta.edu>
2024-11-04 15:16:56 -0600

    full_credit

--- P09/main.cpp
+++ P09/main.cpp
@@ -19,5 +19,31 @@ int main(int argc, char* argv[]) {
         //prompt to the user
         std::cout << "Enter 'q' to quit or press Enter to tick: ";
 
+        while (true) {
+            // Get user input
+            std::getline(std::cin, input);
+
+            if (input == "q") {
+                break;
+            } else {
+                clock.tic();
+            }
+
+            // Print the current time after ticking
+            std::cout << "The time is now ";
+            clock.print();
+            std::cout << std::endl;
+
+            // Get user input again
+            std::cout << "Enter 'q' to quit or press Enter to tick: ";
+        }
+    }catch (const std::out_of_range& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return -2;
+    } catch (const std::invalid_argument&) {
+        std::cerr << "Invalid input format. Please enter integers for hours, minutes, and seconds." << std::endl;
+        return -1;
     }
+
+    return 0;
 }
