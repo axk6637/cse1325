0b086f3b806bbb9953f690e97507df85bc38c9b6
Aashreeya Karmacharya <axk6637@mavs.uta.edu>
2024-11-04 23:28:07 -0600

    bonus

--- P09/bonus/main.cpp
+++ P09/bonus/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stdexcept>
-#include "clock.h"
+#include "timer.h"
 
 int main(int argc, char* argv[]) {
     if (argc != 4) {
@@ -13,11 +13,11 @@ int main(int argc, char* argv[]) {
         int minutes = std::stoi(argv[2]);
         int seconds = std::stoi(argv[3]);
 
-        Clock clock(hours, minutes, seconds);
+        Timer timer(hours, minutes, seconds);
         std::string input;
 
         //prompt to the user
-        std::cout << "Enter 'q' to quit or press Enter to tick: ";
+        std::cout << "Enter 'q' to quit or press Enter to tick/continue: ";
 
         while (true) {
             // Get user input
@@ -26,24 +26,22 @@ int main(int argc, char* argv[]) {
             if (input == "q") {
                 break;
             } else {
-                clock.tic();
+                timer.tic();
             }
 
             // Print the current time after ticking
             std::cout << "The time is now ";
-            clock.print();
+            timer.print();
             std::cout << std::endl;
-
-            // Get user input again
-            std::cout << "Enter 'q' to quit or press Enter to tick: ";
+            
         }
     }catch (const std::out_of_range& e) {
         std::cerr << "Error: " << e.what() << std::endl;
         return -2;
     } catch (const std::invalid_argument&) {
-        std::cerr << "Invalid input format. Please enter integers for hours, minutes, and seconds." << std::endl;
-        return -1;
-    }
+        std::cerr << "Invalid input format: Please enter integers inuts." << std::endl;
+    }catch (const std::runtime_error& e) {
+        std::cout << e.what() << std::endl;  // Catch Timer expiration 
 
     return 0;
 }
